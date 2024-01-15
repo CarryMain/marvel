b20357a270cc08fb168563a77c91ea573502f515
CarryMain <mr.turanov98@gmail.com>
2024-01-15 12:48:07 +0300

    add charInfo

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,3 +1,4 @@
+import { Component } from "react";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharInfo from '../charInfo/CharInfo';
@@ -5,20 +6,33 @@ import CharList from '../charList/CharList';
 
 import decoration from '../../resources/image/UW.png';
 
-const App = () => {
-    return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                <RandomChar/>
-                <div className="char__content">
-                    <CharList/>
-                    <CharInfo/>
-                </div>
-                <img src={decoration} alt="vision" className="bg-decoration" />
-            </main>
-        </div>
-    )
+class App extends Component {
+
+    state = {
+        selectedChar: null,
+    }
+
+    onCharSelected = (id) => {
+        this.setState({
+            selectedChar: id,
+        })
+    }
+    
+    render() {
+        return (
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <RandomChar/>
+                    <div className="char__content">
+                        <CharList onCharSelected={this.onCharSelected}/>
+                        <CharInfo charId={this.state.selectedChar}/>
+                    </div>
+                    <img src={decoration} alt="vision" className="bg-decoration" />
+                </main>
+            </div>
+        )
+    }
 }
 
 export default App;
